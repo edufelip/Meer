d895a80f84e46a99143157a1ea9120b4b729e6c7
EduSantos <eduardofelipi@gmail.com>
2025-12-10 15:25:48 -0300

    core: committing android and ios folders

--- app.config.js
+++ app.config.js
@@ -2,12 +2,7 @@ export default ({ config }) => ({
   ...config,
   android: {
     package: "com.edufelip.meer",
-    googleServicesFile: process.env.GOOGLE_SERVICES_JSON ?? "./config/google-services.json",
-  },
-  extra: {
-    eas: {
-      projectId: "ac66eaa0-2006-4371-8b97-10ce159f01d2"
-    }
+    googleServicesFile: "./android/app/google-services.json",
   },
   plugins: [
     // React Native Firebase Crashlytics config plugin (handles mapping/native symbol upload)
